d37ecb3ac4ab18141d2cb72d0b4a6f3b3d92ec63
Taha <aitabitaha@gmail.com>
2024-07-09 19:17:17 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php 
+$topics = ["Definitions", "Use of the road", "Speeds and distances", "Manouvers", "Priority at level crossigns and tunnels", "Transporting people and cargo", "Lights and indication in the vehicle", "Signs". "Documentation", "The Driver", "The road users", "Vehicle controls, visibility and comfort", "Safe driving", "The vehicles safety devices", "Journeys, efficient and preventive driving", "Driving accidents", "Vehicle maintainace"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>Permiso B - ES Driving Licence</title>
+    <link rel="stylesheet" href="css/style.css" media="all">
+  </head>
+  <body>
+    <header class="header">
+      <h1>Permiso B - Spanish Driving Licence</h1>
+<div class="navigation">
+   <button class="home"><a href="index.php">Home</a></button>
+   <button class="definitions"><a href="views/definitions.html">Definitions</a></button>
+</div>
+    </header>
+    <main>
+      <section class="categories">
+       <h3 class="categories-title">By topic</h3>
+        <ul class="topics">
+<?php
+foreach ($topics as $topic) {
+    echo '<li class="topic_item"><a href="views/' . $topic . '.html">' . $topic . '</a></li>';
+}
+?>
+        </ul>
+      </section>
+    </main>
+  </body>
+</html>
